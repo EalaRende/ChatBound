50bf7b734f072ba413941233defcaa8cf45d5037
EalaRende <esheahm@gmail.com>
2026-09-18 10:26:13 +0200

    Do not filter Pet-authored chat

--- ChatFilterService.cs
+++ ChatFilterService.cs
@@ -16,7 +16,10 @@ public sealed class ChatFilterService
     public bool TryFilter(IHandleableChatMessage message)
     {
         var channel = message.LogKind;
-        if (!configuration.Enabled || channel == XivChatType.TellOutgoing || !configuration.Channels.Contains(channel))
+        if (message.SourceKind == XivChatRelationKind.LocalPlayer ||
+            !configuration.Enabled ||
+            channel == XivChatType.TellOutgoing ||
+            !configuration.Channels.Contains(channel))
             return false;
 
         if (!configuration.Profiles.TryGetValue(configuration.ActiveProfile, out var words))
